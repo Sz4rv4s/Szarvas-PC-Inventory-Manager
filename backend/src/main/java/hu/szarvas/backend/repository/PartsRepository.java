1556d66efd5bf141f0a832cc8f718e3d6717ada6
Szarvas Péter <peti20031202@gmail.com>
2024-10-16 22:32:10 +0200

    fix: added a missing import

--- backend/src/main/java/hu/szarvas/backend/repository/PartsRepository.java
+++ backend/src/main/java/hu/szarvas/backend/repository/PartsRepository.java
@@ -1,5 +1,6 @@
 package hu.szarvas.backend.repository;
 
+import hu.szarvas.backend.model.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PartsRepository extends JpaRepository<Part, String> {
--- backend/src/main/java/hu/szarvas/backend/repository/WarehousesRepository.java
+++ backend/src/main/java/hu/szarvas/backend/repository/WarehousesRepository.java
@@ -1,5 +1,6 @@
 package hu.szarvas.backend.repository;
 
+import hu.szarvas.backend.model.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WarehousesRepository extends JpaRepository<Warehouse, Long> {
