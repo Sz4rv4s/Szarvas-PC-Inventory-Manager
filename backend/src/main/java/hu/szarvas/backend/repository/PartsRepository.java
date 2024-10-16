916433c1b2fb7a6a2c0c9bb30c7eb9ff395476ae
Szarvas Péter <peti20031202@gmail.com>
2024-10-16 23:26:34 +0200

    feat: created service layer with methods for creating requests

--- backend/src/main/java/hu/szarvas/backend/repository/PartsRepository.java
+++ backend/src/main/java/hu/szarvas/backend/repository/PartsRepository.java
@@ -3,5 +3,8 @@ package hu.szarvas.backend.repository;
 import hu.szarvas.backend.model.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PartsRepository extends JpaRepository<Part, String> {
+import java.util.List;
+
+public interface PartsRepository extends JpaRepository<Part, Integer> {
+    List<Part> findByModelContainingIgnoreCase(final String name);
 }
--- backend/src/main/java/hu/szarvas/backend/repository/WarehousesRepository.java
+++ backend/src/main/java/hu/szarvas/backend/repository/WarehousesRepository.java
@@ -3,5 +3,8 @@ package hu.szarvas.backend.repository;
 import hu.szarvas.backend.model.Warehouse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WarehousesRepository extends JpaRepository<Warehouse, Long> {
+import java.util.List;
+
+public interface WarehousesRepository extends JpaRepository<Warehouse, Integer> {
+    List<Warehouse> findByNameContainingIgnoreCase(String name);
 }
