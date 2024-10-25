dfef98a23d2022e928df1d829325386e56388573
Szarvas Péter <peti20031202@gmail.com>
2024-10-25 19:33:09 +0200

    fix: added the frontend's address to the cors settings

--- backend/src/main/java/hu/szarvas/backend/config/WebConfig.java
+++ backend/src/main/java/hu/szarvas/backend/config/WebConfig.java
@@ -1,16 +1,22 @@
 package hu.szarvas.backend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping( "/**" )
-                .allowedOrigins("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS");
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
