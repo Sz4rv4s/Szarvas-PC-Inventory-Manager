ce6fc206dcb0e199eae7a0534ba5e3cf991dd62b
Péter Szarvas <peti20031202@gmail.com>
2024-11-14 08:24:21 +0100

    fix: fixed cors to enable frontend to access it locally

--- backend/src/main/java/hu/szarvas/backend/config/WebConfig.java
+++ backend/src/main/java/hu/szarvas/backend/config/WebConfig.java
@@ -13,8 +13,9 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:*")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
